8ded62f251b69c0a55142abd02057d4e96ea883c
LuciLua <luci.lua81@gmail.com>
2021-05-20 19:56:09 -0300

    :page_facing_up: Draft Update | filter with lenght and join + arrow function

--- 01/rascunhos/revisando12.js
+++ 01/rascunhos/revisando12.js
@@ -5,3 +5,15 @@ function imprimeNomes(nome, indice){
 }
 
 arrayNomes.forEach(imprimeNomes)
+
+// filter com arrow function, extraindo 
+const nomeLetraMenorQueCinco = ( nome => nome.length < 5) 
+const nomeLetraMaiorQueCinco = ( nome => nome.length > 5) 
+// agora tenho função que posso reutilizar em outras partes do sistema
+// const nomesCurtos = arrayNomes.filter(nomeLetraMenorQueCinco)
+
+const nomesCurtos = arrayNomes.filter(nomeLetraMenorQueCinco).join('\n')
+const nomesLongos = arrayNomes.filter(nomeLetraMaiorQueCinco).join('\n')
+console.log(`\n----------\nNomes curtos:\n${nomesCurtos} \n--------`)
+console.log(`\n----------\nNomes Longos:\n${nomesLongos} \n--------`)
+
