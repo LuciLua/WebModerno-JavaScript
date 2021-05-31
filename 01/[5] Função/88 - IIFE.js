86965b3f9401d077b11bcb5ca5d5718d33993988
LuciLua <luci.lua81@gmail.com>
2021-05-31 10:34:24 -0300

    :star2: Funções | [88] IIFE

---
+++ 01/[5] Função/88 - IIFE.js
@@ -0,0 +1,17 @@
+// função autoinvocada
+// fugir de escopo global
+// função anonima
+
+// se algo é comaprtilhado com toda sua aplicação,
+// principalmente se é algo que é variavel,
+// muitos codigos (que rodam no seu browser) podem manipular aquele valor variavel e pode gerar bugs inesperados
+
+// IIFE -> expressão de função / Function expression
+// Immediately Invoked Function Expression
+
+(function(){
+    console.log
+    ('Será executado na hora!')
+    console.log('Fugindo do escopo mais abrangente!')
+})()
+
