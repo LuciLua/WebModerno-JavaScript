4fedecd2b4ffcb98eafe933ae7f9e51f7c8f8946
LuciLua <luci.lua81@gmail.com>
2021-06-24 16:27:03 -0300

    :pencil: Funções | Exercicio 10

---
+++ 01/[5] Função/exercícios/10 - exercicio.js
@@ -0,0 +1,13 @@
+function diviTres (num){
+    if (num % 3 === 0){
+        return console.log(true)
+    } else{
+        return console.log(false)
+    }
+}
+// se numero é divisivel por 3
+diviTres(9)
+diviTres(6)
+diviTres(81)
+diviTres(7)
+diviTres(34)
