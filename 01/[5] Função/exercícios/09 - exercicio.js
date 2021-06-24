81e7476a2cfc4346dc7496f38876684c2cfc205f
LuciLua <luci.lua81@gmail.com>
2021-06-24 16:20:50 -0300

    :pencil: Funções | Exercicio 9 v3

--- 01/[5] Função/exercícios/09 - exercicio.js
+++ 01/[5] Função/exercícios/09 - exercicio.js
@@ -1,30 +1,25 @@
 let notas = [2, 33, 35, 30, 67, 99 , 27, 45, 55, 58, 68, 38, 37]
 
-let multiplosJa = []
-let multiploDepois = []
+let aprovados = []
 let reprovados = []
 
-for (let i in notas){
-    if (notas[i] >= 38){
-        if (notas[i] % 5 > 0){
-            multiploDepois.push('[A] Nota arredondada: ' + (notas[i] + (5 - (notas[i] % 5))))
-        }
-        else{
-            multiplosJa.push('[A] Nota exata: ' + notas[i])
+function situacao (){
+    for (let i in notas){
+        if (notas[i] >= 38){
+            if (notas[i] % 5 > 0){
+                aprovados.push('[A] Nota: ' + (notas[i] + (5 - (notas[i] % 5))))
+            } else{
+                aprovados.push('[A] Nota: ' + notas[i])
+            }
+        } else{
+            reprovados.push('[R] Nota: ' + notas[i])
         }
     }
-    else{
-        (reprovados.push('[R] Nota:' + notas[i]))
-    }
+
+var t = '--------------------------------'
+console.log(`${t}\n[A] - Aprovados | [R] Reprovados\n${t}
+${aprovados.join('\n')}\n${t}
+${reprovados.join('\n')}\n${t}`)
 }
-console.log(`
---------------------------------
-[A] - Aprovados | [R] Reprovados
---------------------------------
-${multiplosJa.join('\n')}
---------------------------------
-${multiploDepois.join('\n')}
---------------------------------
-${reprovados.join('\n')}
---------------------------------
-`)
+
+ situacao()
