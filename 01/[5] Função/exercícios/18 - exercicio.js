8d1e2724318c28152576136be983722b26c6ec45
LuciLua <luci.lua81@gmail.com>
2021-06-28 12:55:19 -0300

    :pencil: Funções | Exercicio 18

---
+++ 01/[5] Função/exercícios/18 - exercicio.js
@@ -0,0 +1,41 @@
+function numeroEntreZeroDez(numero){
+    switch (numero){
+        case 0:
+            console.log('Zero')
+            break
+        case 1:
+            console.log('Um')
+            break
+        case 2:
+            console.log('Dois')
+            break
+        case 3:
+            console.log('Três')
+            break
+        case 4:
+            console.log('Quatro')
+            break
+        case 5:
+            console.log('Cinco')
+            break
+        case 6:
+            console.log('Seis')
+            break
+        case 7:
+            console.log('Sete')
+            break
+        case 8:
+            console.log('oito')
+            break
+        case 9:
+            console.log('Nove')
+            break
+        case 10:
+            console.log('Dez')
+            break
+        default:
+            console.log('Número fora do intervalo')
+    }
+}
+
+numeroEntreZeroDez(8)
