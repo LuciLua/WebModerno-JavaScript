873a672dea2447fa227b757506ccfc35f3e52586
LuciLua <luci.lua81@gmail.com>
2021-06-29 02:24:17 -0300

    :pencil: Funções | Exercicio 22 (with invalid month)

--- 01/[5] Função/exercícios/22 - exercicios.js
+++ 01/[5] Função/exercícios/22 - exercicios.js
@@ -5,18 +5,27 @@ function valorPagar(mes, valorAnuidade){
     let valorFinal = AnuidadeFixaComMulta + valorFixo
 
     const meses = [mes[1],mes[2],mes[3],mes[4],mes[5],mes[6],mes[7],mes[8],mes[9],mes[10],mes[11],mes[12]] = [null,'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-    console.log('Mes: ', meses[mes])
-    
-    if (mes > 1){
-        for (i = 0; i <= meses.indexOf(meses[mes-2]); i++){
-            AnuidadeFixaComMulta  += (5/100)*valorAnuidade // anuidade = + 5% do valor da anuidade por mes depois de janeiro (pois é considerado atraso)
+
+    if (mes > 0 && mes < 13){
+        if (mes > 1){
+            for (i = 0; i <= meses.indexOf(meses[mes-2]); i++){
+                AnuidadeFixaComMulta  += (5/100)*valorAnuidade // anuidade = + 5% do valor da anuidade por mes depois de janeiro (pois é considerado atraso)
+            }
+        } else{
+            console.log('janeiro não paga multa, portanto, apenas pagará o valor fixo da anuidade que é', valorAnuidade)
         }
+        
+        valorFinal = AnuidadeFixaComMulta + valorFixo
+
+        console.log('Mes: ', meses[mes])
+        console.log(`Valor: R$${valorFinal}`)
     } else{
-        console.log('janeiro não paga multa, portanto, apenas pagará o valor fixo da anuidade que é', valorAnuidade)
+        console.log('Mês informado Inválido')  
+        mes = null     
     }
-    valorFinal = AnuidadeFixaComMulta + valorFixo
-    console.log(`Valor: R$${valorFinal}`)
 }
+    
+    
 
 console.log('---------')
 
@@ -29,3 +38,15 @@ valorPagar(4,50)
 console.log('---------')
 
 valorPagar(4,5) // mes 4 com multa de 5 por cento de 5 por 3 meses mais o 5 de anuidade 
+
+console.log('---------')
+
+valorPagar(12,5) 
+
+console.log('---------')
+
+valorPagar(13,5) 
+
+console.log('---------')
+
+valorPagar(0,5) 
