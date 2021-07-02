875a0fc999a40c28db9b7bcd530ca7993b751f06
LuciLua <luci.lua81@gmail.com>
2021-07-01 22:02:55 -0300

    :pencil: Funções | Exercicio 27 if altura for igual, else exibe igual

--- 01/[5] Função/exercícios/27 - exercicio.js
+++ 01/[5] Função/exercícios/27 - exercicio.js
@@ -1,26 +1,42 @@
 function criancas(altA, taxA, altB, taxB){
 
-    function Crianca(altura, taxa){
+    function Crianca(nome, altura, taxa){
+        this.nome = nome
         this.altura = altura
         this.taxa = taxa
-        this.info = function(){ console.log('Altura: ',this.altura, '\nTaxa:', this.taxa)}
+        this.info = function(){ console.log('Criança',this.nome,'\nAltura: ',this.altura, '\nTaxa:', this.taxa, '\n')}
     }
 
-    const criancaAObj = new Crianca(altA, taxA)
-    const criancaBObj = new Crianca(altB, taxB)
+    const criancaAObj = new Crianca('Felipe', altA, taxA)
+    const criancaBObj = new Crianca('Milena', altB, taxB)
 
     criancaAObj.info()
     criancaBObj.info()
 
     const criancasAll = [criancaAObj, criancaBObj]
 
-    var criancaMaior = 0
+    // const criancaMaior = [0, 'Nome']
+    const criancaMaior = []
+    
     for (let i = 0; i < criancasAll.length; i++){
-        while (criancaMaior < criancasAll[i].altura){
-            criancaMaior = criancasAll[i].altura
-        }        
+
+        if (criancasAll[i].altura !== criancasAll[0].altura || criancasAll[i].altura !== criancasAll[1].altura){
+            criancaMaior.push((criancasAll[i].altura), criancasAll[i].nome)
+    
+            while (criancaMaior[0] > criancasAll[i].altura){
+                criancaMaior[0] = criancasAll[i].altura
+                criancaMaior[1] = (criancasAll[i].nome)
+            }        
+
+        } 
+        
+        else {
+            criancaMaior[0] = 'Alturas iguais'
+            criancaMaior[1] = 'Alturas iguais'
+        }
+        
     }
-    console.log('Altura da maior criança: ',criancaMaior)
+    console.log('Altura da menor criança: ',criancaMaior[0], '\nNome:', criancaMaior[1])
 }
 
-criancas(23,4,56,7)
+criancas(7,4,9,7) // crianca A: alt=23 tax=4 | crianca B: alt=56 tax=7
