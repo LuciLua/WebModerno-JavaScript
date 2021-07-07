4a8c0dcb3f1b88a54d5e3ebf0629a31742edb655
LuciLua <luci.lua81@gmail.com>
2021-07-07 20:24:14 -0300

    :pencil: Funções | Exercicio 38 (OK)

--- 01/[5] Função/exercícios/38 - exercicio.js
+++ 01/[5] Função/exercícios/38 - exercicio.js
@@ -1,11 +1,19 @@
-function func(inicio, fim){
+function func(inicio = 0, fim = 100){
     const impares = []
+    if (inicio > fim){
+        inicio = fim + inicio
+
+        fim = inicio - fim // transforma fim em inicio retirando o fim antigo do novo inicio
+        inicio = inicio - fim // coloca o valor verdadeiro para o inicio 
+
+        console.log('transformado!')
+    }
     for (let i = inicio; i <= fim; i++){
-        if (i%2 != 0){
+        if (i%2 == 1){
             impares.push(i)
         }
     }
     return console.log(impares)
 }
 
-func(0,100)
+func(100,0)
