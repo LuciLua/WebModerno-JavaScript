8399b5d588369984e1e8e050a153df9d629add2c
LuciLua <luci.lua81@gmail.com>
2021-07-08 13:23:59 -0300

    :page_facing_up: Draft Update | for dentro de for

--- 01/rascunhos/revisando24.js
+++ 01/rascunhos/revisando24.js
@@ -14,3 +14,19 @@ function inverte (min, max){
 
 inverte(0, 100)
 inverte(50, 10)
+
+function letras (nome,idade){
+    for (let  i = 0; i <= idade; i++){
+        console.log(nome,i)
+        for (let o = 0; o < nome.length; o++){
+            console.log(nome.length, 'letras nome: ', nome, 'na posição :', i)
+
+            for (let p = 0; p < 2; p++){
+                console.log('test', p)
+            }
+        }
+        console.log('------')
+    }
+}
+
+letras('lu',5)
