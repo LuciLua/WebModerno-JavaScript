8ecc97199b2fea1eb822db5263835c57926fe20d
LuciLua <luci.lua81@gmail.com>
2021-08-14 15:49:00 -0300

    :star2: [142] - reduce3

---
+++ 01/[7] Array/142 - reduce3.js
@@ -0,0 +1,15 @@
+//implementar reduce
+
+Array.prototype.reduce2 = function(callback, valorInicial){
+    const indiceInicial = valorInicial ? 0 : 1
+    let acumulador = this[0] || valorInicial
+    for(let i = indiceInicial; i < this.length; i++){
+        acumulador = callback(acumulador, this[i], i, this)
+    }
+    return acumulador
+}
+
+const soma = (total, valor) => total + valor
+const nums = [1,2,3,4,5, 6]
+
+console.log(nums.reduce2(soma, 1))
