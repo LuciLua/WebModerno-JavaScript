c7e9c4db4ed612cb3bb22db236329409a611873f
LuciLua <luci.lua81@gmail.com>
2021-08-18 18:18:07 -0300

    :pencil: JS | Exercicio 6

--- 01/[9] Exercicios/6 - exercicio.js
+++ 01/[9] Exercicios/6 - exercicio.js
@@ -14,3 +14,16 @@ inverso('dl')
 inverso(true)
 inverso(false)
 inverso(3)
+
+// resolution 2
+
+function inverso2(BolNum){
+    if (typeof BolNum == "number"){console.log(typeof BolNum, ':', -BolNum)}
+    else if (typeof BolNum == "boolean"){console.log(typeof BolNum, ':', !BolNum)}
+    else {console.log("booleano ou número esperados, mas o parâmetro é do tipo: "+typeof BolNum)
+}}
+
+inverso2('dl')
+inverso2(true)
+inverso2(false)
+inverso2(3)
