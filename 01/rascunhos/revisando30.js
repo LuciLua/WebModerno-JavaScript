09bc39f7d5a0213ff9433f8f9619db84f5fd2c24
LuciLua <luci.lua81@gmail.com>
2021-08-22 16:23:59 -0300

    :page_facing_up: Draft Update | filter: condicional

--- 01/rascunhos/revisando30.js
+++ 01/rascunhos/revisando30.js
@@ -7,3 +7,16 @@ const array = ['uva', 'maça', 'banana', 'pera', 'banana']
 const buscar = 'banana'
 
 teste(array, buscar)
+
+
+function bool(numeros) {
+    return numeros.filter((nome, indice) => {
+        const nomeComL = nome === nome.toUpperCase() //boolean (todos maiusculos)
+        const indice2 = indice%2 === 0  //boolean (APENAS COM INDICE PAR)
+        const maiorque = nome.length > 5  //boolean (tem q ter mais de 5 caracteres)
+        return maiorque && nomeComL && indice2 // so retorna se os 2 for true
+    })
+}
+
+console.log(bool(['ola', 'nao', 'Lucia', 'asadasd', 'PARPARP']))
+console.log(bool(['aaa', 'Lucia', 'AAA', 'nao', 'SIM', 'SEM INDICE PAR', 'COM INDICE PAR']))
