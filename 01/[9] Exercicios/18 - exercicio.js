2e02808a946a920e79a5e0c459b40ac695de359d
LuciLua <luci.lua81@gmail.com>
2021-08-28 16:11:35 -0300

    :pencil: JS | Exercicio 18

--- 01/[9] Exercicios/18 - exercicio.js
+++ 01/[9] Exercicios/18 - exercicio.js
@@ -13,3 +13,66 @@ despesasTotais([
     {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
     {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
     ]) // retornará 34599.89
+
+
+
+
+
+//outras resoluções
+
+
+function despesasTotaisD(itens) {
+    return itens.reduce((acumulador, valorAtual) => acumulador + valorAtual.preco, 0)
+}
+
+
+function despesasTotaisT(itens) {
+    console.log(itens
+    .map(item => item.preco)
+    .reduce((acumulador, valorAtual) => acumulador + valorAtual))
+}
+
+
+function despesasTotaisQ(itens) {
+    var total = 0
+    for (let item of itens) total += item.preco
+    console.log(total)
+}
+
+
+
+
+
+
+despesasTotaisD([
+    {nome: "Jornal online", categoria: "Informação", preco: 89.99},
+    {nome: "Cinema", categoria: "Entretenimento", preco: 150}
+    ]) // retornará 239.99
+
+despesasTotaisD([
+    {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
+    {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
+    ]) // retornará 34599.89
+
+
+
+despesasTotaisT([
+    {nome: "Jornal online", categoria: "Informação", preco: 89.99},
+    {nome: "Cinema", categoria: "Entretenimento", preco: 150}
+    ]) // retornará 239.99
+
+despesasTotaisT([
+    {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
+    {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
+    ]) // retornará 34599.89
+
+
+despesasTotaisQ([
+    {nome: "Jornal online", categoria: "Informação", preco: 89.99},
+    {nome: "Cinema", categoria: "Entretenimento", preco: 150}
+    ]) // retornará 239.99
+
+despesasTotaisQ([
+    {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
+    {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
+    ]) // retornará 34599.89
