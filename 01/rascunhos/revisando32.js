d49615da81295515013d02f072662981aea2085d
LuciLua <luci.lua81@gmail.com>
2021-09-09 19:58:13 -0300

    :page_facing_up: Draft Update | revise: reduce and '...'element

---
+++ 01/rascunhos/revisando32.js
@@ -0,0 +1,8 @@
+
+const arrayNum = [20, 33, 24, 12, 98, 70]
+console.log(Math.min(...arrayNum))
+console.log(Math.max(...arrayNum))
+
+
+const arrayEl = ['a', 'b', 'c', 'd', 'e']
+console.log(arrayEl.reduce((ant, atu) => ant + 1 + atu)) 
