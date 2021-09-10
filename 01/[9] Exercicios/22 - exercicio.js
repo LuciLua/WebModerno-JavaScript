96fb908a258a54789f28ae6df805964fbe930e4d
LuciLua <luci.lua81@gmail.com>
2021-09-10 15:55:13 -0300

    :pencil: JS | Exercicio 22

--- 01/[9] Exercicios/22 - exercicio.js
+++ 01/[9] Exercicios/22 - exercicio.js
@@ -10,3 +10,22 @@ function funcaoDaSorte(numero){
 funcaoDaSorte(10)
 funcaoDaSorte(5)
 funcaoDaSorte(5)
+
+
+// outra resolucao
+
+/*mais informações sobre gerar números aleatórios dentro de uma certa faixa
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+function funcaoDaSorte2(numeroEscolhido) {
+    const min = 1
+    const max = 10
+    const numeroAleatorio = Math.floor(Math.random() * (max - min + 1) + min)
+    return (numeroEscolhido === numeroAleatorio ?
+    `Parabéns! O número sorteado foi o ${numeroAleatorio}` :
+    `Que pena, o número sorteado foi o ${numeroAleatorio}`
+    )
+}
+
+console.log(funcaoDaSorte2(10))
+console.log(funcaoDaSorte2(5))
+console.log(funcaoDaSorte2(5))
