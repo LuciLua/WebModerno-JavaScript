b1d26169bf4b78fbdf42ba7eaac0a01e68d49bc5
LuciLua <luci.lua81@gmail.com>
2021-09-11 19:38:41 -0300

    :pencil: JS | Exercicio 24

--- 01/[9] Exercicios/24 - exercicio.js
+++ 01/[9] Exercicios/24 - exercicio.js
@@ -1,6 +1,6 @@
 function contarCaractere(letra, string){
     var quantidadeLetra = 0
-    for(let i = 0; i < string.split('').length; i++){
+    for(let i = 0; i < string.length; i++){
         if (string.split('')[i] === letra) quantidadeLetra++
     }
     console.log(quantidadeLetra)
@@ -9,3 +9,33 @@ function contarCaractere(letra, string){
 contarCaractere("r", "A sorte favorece os audazes") // retornará 2
 contarCaractere("c", "Conhece-te a ti mesmo") // retornará 1
 contarCaractere("x", "Conhecxe-te xa ti x mexsXXmo") // retornará 4
+
+
+console.log('----------------------------------')
+//outras resolucoes
+
+function contarCaractere2(caractereBuscado, frase) {
+    let contador = 0
+    for (let i = 0; i < frase.length; i++)
+        if (frase.charAt(i) === caractereBuscado) contador++ 
+        console.log(contador)
+}
+// string.split('')[i] = string.charAt(i)
+
+
+contarCaractere2("r", "A sorte favorece os audazes") // retornará 2
+contarCaractere2("c", "Conhece-te a ti mesmo") // retornará 1
+contarCaractere2("x", "Conhecxe-te xa ti x mexsXXmo") // retornará 4
+
+
+//outra resolucao
+console.log('----------------------------------')
+
+function contarCaractere3(caractereBuscado, frase) {
+    console.log ([...frase].filter(caractere => caractere === caractereBuscado).length)
+}
+
+
+contarCaractere3("r", "A sorte favorece os audazes") // retornará 2
+contarCaractere3("c", "Conhece-te a ti mesmo") // retornará 1
+contarCaractere3("x", "Conhecxe-te xa ti x mexsXXmo") // retornará 4
