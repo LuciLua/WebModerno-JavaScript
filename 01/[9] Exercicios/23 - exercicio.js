cf42b94285b043c7e89490a1827b224350ca7c7f
LuciLua <luci.lua81@gmail.com>
2021-09-11 16:20:31 -0300

    :pencil: JS | Exercicio 23

---
+++ 01/[9] Exercicios/23 - exercicio.js
@@ -0,0 +1,4 @@
+const contarPalavras = string => console.log(`${string.split(' ').length} letras`)
+
+contarPalavras("Sou uma frase") // retornará 3
+contarPalavras("Me divirto aprendendo a programar") // retornará 5
