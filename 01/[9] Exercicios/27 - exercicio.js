7cf77ea03481076dd2e6d856af04ea4f56645156
LuciLua <luci.lua81@gmail.com>
2021-09-18 17:09:01 -0300

    :pencil: JS | Exercicio 27 (fromEntries?)

--- 01/[9] Exercicios/27 - exercicio.js
+++ 01/[9] Exercicios/27 - exercicio.js
@@ -1,6 +1,5 @@
 function inverter(obj){
     const newObj = {}
-    console.log(Object.entries(obj))
     Object.entries(obj).forEach(keyValue => {
         const key = 0,
         value = 1
@@ -12,3 +11,15 @@ function inverter(obj){
 }
 
 inverter({ a: 1, b: 2, c: 3}) // retornará { 1: "a", 2: "b", 3: "c"}
+
+// Solucao 2
+
+function inverter2(objeto) {
+    const paresDeChaveValorInvertidos = Object.entries(objeto)
+    .map( parChaveValor => parChaveValor.reverse() )
+
+    console.log( Object.fromEntries(paresDeChaveValorInvertidos)
+    ) // fromEntries?
+}
+
+inverter2({ a: 5, b: 2, c: 9}) // retornará { 1: "a", 2: "b", 3: "c"}
