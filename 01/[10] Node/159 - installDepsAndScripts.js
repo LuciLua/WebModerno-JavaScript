af0ad9c36274d5971007675037a6a25979d6b560
LuciLua <luci.lua81@gmail.com>
2021-09-22 19:11:11 -0300

    :star2: [159] - installDepsAndScripts And RecriandoFolderNode_modules

---
+++ 01/[10] Node/159 - installDepsAndScripts.js
@@ -0,0 +1,32 @@
+// package.json
+/**
+ * da pra criar scripts dentro desse arquivo para executar de forma simples pelo terminal
+ * 
+ * da pra recriar pasta node_modules a partir do q ta definido nas dependencias do arquivo package.json
+ * 
+ * package.josn: descritor com as dependencias
+ */
+
+
+//  "start": "nodemon", (ja vai procurar arquivo principal que coloquei dentro da propriedade main)
+
+// npm i
+
+// recriando pasta node-modules
+// por isso nao precisa salvar essa pasta dentro do repositorio
+
+// npm dev nao vai dar certo pois start é um comando padrao do node
+// entoa precisa:
+// nom run dev
+
+
+// 
+// "test": "echo \"Error: no test specified\" && exit 1" // aparece mensagem a mais
+
+// "test": "echo \"Error: no test specified\" && exit 0" // nao aparece mensagem a mais
+
+
+// comandos encadeados com '&&' comercial
+
+
+// https://docs.npmjs.com/cli/v7/using-npm/scripts
