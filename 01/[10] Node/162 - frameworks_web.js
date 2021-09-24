35d3735f448e6a2160fe1aa99d961997b21c18ee
LuciLua <luci.lua81@gmail.com>
2021-09-24 11:47:38 -0300

    :star2: [162] - frameworksWeb

---
+++ 01/[10] Node/162 - frameworks_web.js
@@ -0,0 +1,33 @@
+// express.js
+// para fazer web services da aplicação
+
+// codigo que roda no servidor X codigo que é interpretado pelo browser
+
+// biblioteas (lodash, axios...)
+// conjunto de funcionaldiade, funções prontas para serem usadas
+
+
+// já o framework é algo que estrutura a aplicação, ele da a base, estrutura para
+// desenvolver a aplicação seguindo os preceitos que aquele framework estabelece
+
+// ex: angular, vue
+
+// express: framework para contruir webservices do lado do client
+
+// tem framework do front end e do back end
+
+// framworks de persistencia para persistir dados no banco de dados..
+// frameworks de criação de web services
+// frameworks de como contruir web services..
+
+// ---------------------------
+
+// framework: algo que vc controi a aplicacao em cima
+// biblioteca: conjunto de funcionalidades, bibliotecla pranta
+
+
+// pode usar um conjunto de framework
+
+
+// padrao de projeto> chain of responsibility
+// express altamente baseado nesse padrão
