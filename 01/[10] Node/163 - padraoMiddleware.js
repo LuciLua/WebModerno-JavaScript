2f85d7535a465e1d221b093a0553f88bf5f5f364
LuciLua <luci.lua81@gmail.com>
2021-09-24 11:57:38 -0300

    :star2: [162] - padrao middleware

---
+++ 01/[10] Node/163 - padraoMiddleware.js
@@ -0,0 +1,16 @@
+// padrão middleware = chain of responsability
+// muito presente no node, no express...
+// ----------------
+
+
+// chain of responsability:  separar o processo em pequenos passos
+// sem que elas precisem uma da outra, independencia, 
+// sem precusasr de acoplamento de um passo com outros
+
+// cadeia de passos sendo executada 
+// passos independentes, posso no final de um chamar uma função next()
+// para começar o proximo passo da outra
+// posso ou nao chamar o next()
+
+
+// para: reusar
