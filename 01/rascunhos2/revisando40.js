06bf9ebe3e9be843d86eb573bea8f64c79b7469e
LuciLua <luci.lua81@gmail.com>
2021-09-29 16:45:56 -0300

    :page_facing_up: Draft Update | spread and concat arrays inside array

--- 01/rascunhos2/revisando40.js
+++ 01/rascunhos2/revisando40.js
@@ -6,3 +6,7 @@ list.fill('item')
 list.push('ola2')
 
 console.log(list)
+
+const arrays = [['nome', 'idade', 'cep'], ['OLA', 'TCHU', 'HOU'], ['oasd', 'fklfk', 'kffff']]
+const novo = [].concat(...arrays)
+console.log(novo) // concatenando itens de array dentro de array com operador spread ...
