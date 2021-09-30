5f2f6fa4603e347b9846b0dc0b7024c1b9baac1e
LuciLua <luci.lua81@gmail.com>
2021-09-30 14:13:05 -0300

    :star2: [191] - desafio1 -resolucao

--- 01/[11] ESNext/191 - desafioPromise.js
+++ 01/[11] ESNext/191 - desafioPromise.js
@@ -1,24 +1,29 @@
 const fs = require('fs')
+const { type } = require('os')
 const path = require('path')
 
 // colocar dentro de uma promise
 // passar para a promise um path 
 // resultado da promise: conteudo do arquivo
 
+
 function lerArquivo(arquivo){
+    
+    const caminho = path.join(__dirname, arquivo)
+    console.log('Caminho: ', caminho)
+
     return new Promise(resolve => {
-
-        const caminho = path.join(__dirname, arquivo)
-        console.log('Caminho: ', caminho)
-
-        fs.readFile(caminho, 'utf-8', (erro, conteudo) => {
-            resolve(conteudo)
+        fs.readFile(caminho, 'utf-8', (erro, conteudo) => { // caminho, 'formatacao', (erro , conteudo)
+            resolve(conteudo.toString())
         })
     })
 }
 
 lerArquivo('./dados.txt')
-.then(content => console.log(`Conteúdo:
-${content}`))
-.catch(erro => console.log('Ocorreu um erro!', erro))
+.then(content => content.split('\n'))
+.then(linhas => linhas.join(','))
+.then(imprime => `Valor final é:\n${imprime}`)
+.then(console.log)
+
+.catch(erro => console.log('Ocorreu um erro!\n', erro))
 
