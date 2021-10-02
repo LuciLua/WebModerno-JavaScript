680db13def95f7756d40d8c6c3c7b6643dd4cd13
LuciLua <luci.lua81@gmail.com>
2021-10-02 16:51:49 -0300

    :page_facing_up: Draft Update | tratando erros em promises

--- 01/rascunhos2/revisando41.js
+++ 01/rascunhos2/revisando41.js
@@ -1,11 +1,41 @@
-let q = new Promise(cp => {
-    cp({
-        x: 3,
-        y: 4
-    })
-})
+function q(valor){
 
-q
-.then(a => (a.x + a.y))
-.then(v => console.log(v))
-.catch(erro => console.log('erro: ', erro))
+    return new Promise((resolve, reject) => {
+
+
+        // qualquer erro aqui, leva para o .catch()
+        
+
+        try{
+            
+            conso.log('Sou erro, que chamara reject')
+
+            const obj = {
+                x: 3,
+                y: 4
+            }
+
+            var soma = obj.x + obj.y + valor
+
+            if(valor == 12){
+                reject('ErroOcurred: Valor precisa ser 12!')
+
+            } else {
+                resolve(`valor: ${valor} | x: ${obj.x} | y: ${obj.y} | soma: ${soma}`)
+            }
+
+            
+        } 
+
+        catch(e){
+            reject('Tratando meu erro aqui: (' + e + ')')
+        }
+
+    })
+}
+
+q(14)
+// q(12)
+
+.then(console.log)
+.catch(erro => console.log('nou nou! ocorreu erro => ', erro))
