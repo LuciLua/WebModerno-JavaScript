18ab98b2bad4cab7b25b7bac4d411d499cb567b8
LuciLua <luci.lua81@gmail.com>
2021-10-12 18:13:44 -0300

    :star2: [227] - DesafioSlider01

--- 01/[13] HTML/212/exercicios/slider/js/app.js
+++ 01/[13] HTML/212/exercicios/slider/js/app.js
@@ -1,16 +1,21 @@
-function pageScroll(){
-    var slider = document.querySelector('[wm-slider]')
-    var widthScroll = slider.scrollLeft 
 
-    slider.scrollBy(5,0);
-    scrollDelay = setTimeout(pageScroll,20);
-
-    console.log(widthScroll)
-    
-    if(widthScroll == 3000){ // ???? 
-        slider.scrollLeft = 0
+function mover(e, inicio, fim, passo, callback){
+    const novoInicio = inicio - passo
+    // passo: de quanto em quanto (px por exemplo)
+    if(novoInicio >= fim){
+        e.style.left = novoInicio + 'px'
+        setTimeout( () => mover(e, novoInicio, fim, passo, callback), 7)
+    } else {
+        callback()
     }
-
+    
 }
 
-pageScroll()
+const div = document.querySelector('[wm-slider] > div')
+// 1ª prop div dentro de wm-slider: primeira elementSlider
+
+div.style.display = 'block'
+
+
+
+mover(div, 1500, -1500, 5, () => { console.log('terminou') })
