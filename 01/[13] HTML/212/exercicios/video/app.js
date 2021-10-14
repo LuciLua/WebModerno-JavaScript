2940455b82ad72150f3c37f3fb593523373cb0f5
LuciLua <luci.lua81@gmail.com>
2021-10-13 21:51:52 -0300

    :star2: [230] - Video Controller: Desafio feito e melhor

--- 01/[13] HTML/212/exercicios/video/app.js
+++ 01/[13] HTML/212/exercicios/video/app.js
@@ -1,43 +1,47 @@
-const video = document.getElementById('meuVideo')
+function controller(id){
 
-var btnPlay = document.querySelector('[wm-play] > img')
-var btnPause = document.querySelector('[wm-pause] > img')
+    var btnPlay = document.querySelector(`[wm-play=${id}] > img`)
+    var btnPause = document.querySelector(`[wm-pause=${id}] > img`)
+    
+    const video = document.getElementById(id)
 
-function controller(){
-    btnPlay.onclick = play
-    btnPause.onclick = pause
+    video.oncontextmenu = () => false // sem interacao com click
+
+    btnPlay.onclick = () => play()
+    btnPause.onclick = () => pause()
+
+    function progress(){
+        var progressBar = document.querySelector(`[wm-progresso=${id}] > div`)
+        var spanProgress = document.querySelector(`[wm-progressoSpan=${id}]`)
+        setInterval( () =>  {
+            // var atualPorcentagem = ((video.currentTime*100)/video.duration)
+            // tanto faz \/ ou /\
+            var atualPorcentagem = ((video.currentTime/video.duration)*100)
+            progressBar.style.width = `${atualPorcentagem}%`
+            spanProgress.innerHTML = `${atualPorcentagem.toFixed(0)} %`
+        }, 50)
+    }
+
+    function playActive(){
+        btnPause.classList.remove('active')
+        btnPlay.classList.add('active')
+    }
+
+    function pauseActive(){
+        btnPause.classList.add('active')
+        btnPlay.classList.remove('active')
+    }
+
+    function play(){
+        video.play()
+        playActive()
+        progress()
+    }
+
+    function pause(){
+        video.pause()
+        pauseActive()
+    }
 }
 
-function progress(){
-    var progressBar = document.querySelector('[wm-progresso] > div')
-    setInterval( () =>  {
-        var progressTotal = video.duration
-        var progressAtual = video.currentTime
-        var atualPorcentagem = (progressAtual*100)/progressTotal
-        
-        progressBar.style.width = `${atualPorcentagem}%`
-    }, 10)
-}
-
-function playActive(){
-    btnPause.classList.remove('active')
-    btnPlay.classList.add('active')
-}
-
-function pauseActive(){
-    btnPause.classList.add('active')
-    btnPlay.classList.remove('active')
-}
-
-function play(){
-    progress()
-    video.play()
-    playActive()
-}
-
-function pause(){
-    video.pause()
-    pauseActive()
-}
-
-controller(video)
+controller('meuVideo')
