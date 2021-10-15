50115b8f290295ac00e53be42eb14954ad63a31e
LuciLua <luci.lua81@gmail.com>
2021-10-15 19:57:03 -0300

    :star2: [236] - criandoClasseTag

---
+++ 01/[14] CSS/css/js/tag.js
@@ -0,0 +1,35 @@
+const colors = {
+    p: '#388e3c',
+    div: '#1565c0',
+    span: '#e53935',
+    section: '#f67809',
+    ul: '#5e35b1',
+    ol: '#fbc02d',
+    header: '#d81b60',
+    nav: '#64dd17',
+    main: '#00aacc1',
+    footer: '#304ffe',
+    form: '#9f6581',
+    body: '#25b6da',
+    padrao: '#616161',
+    get(tag){
+        return this[tag] ? this[tag] : this.padrao
+    }
+}
+
+
+document.querySelectorAll('.tag').forEach( elemento => {
+
+    const tagName = elemento.tagName.toLowerCase()
+
+    elemento.style.borderColor = colors.get(tagName)
+
+    if (!elemento.classList.contains('nolabel')){ // se nao tiver contida essa classe css (nolabel) nesse elemento (elemento) 
+        const label = document.createElement('label') //criando tag no javascript
+        label.style.backgroundColor  = colors.get(tagName)
+        label.innerHTML = tagName
+
+        elemento.insertBefore(label, elemento.childNodes[0]) // inserindo elemento criado (label) dentro de cada filho desses elmentos, filho na posicao 0
+
+    }
+})
