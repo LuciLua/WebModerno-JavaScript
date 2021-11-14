8791901eeb74af494b073d533f1f2ac3654a98a5
LuciLua <luci.lua81@gmail.com>
2021-11-14 19:54:47 -0300

    :star2: [301] - XMLHttpRequests 02

--- 01/[16] Ajax/ajax/server.js
+++ 01/[16] Ajax/ajax/server.js
@@ -12,7 +12,33 @@ app.use(express.static('.')) // provê arquivos estaticos a partir da aplicaocao
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
+// app.get('/teste', (req, res) => res.send('Ok'))
+
+const multer = require('multer')
+
+// funcao diskStorage qu recebe um objeto que vai configurar a
+// pasta onde vou salvar os arquivos, e o nome do arquivo no momento de salvar ele
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo') // nome do (campo)input q vai vim na requisicao
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.end('Ocorreu um erro')
+        }
+        res.end('Concluido com sucesso!')
+    })
+})
+
 
 app.listen(8080, () => console.log('Executando...'))
 
