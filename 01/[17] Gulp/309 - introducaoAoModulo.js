496316e996d999f1bbe0361eed86ee9c8c44cf17
LuciLua <luci.lua81@gmail.com>
2021-11-19 10:52:59 -0300

    :star2: [309] - Introdução ao módulo

---
+++ 01/[17] Gulp/309 - introducaoAoModulo.js
@@ -0,0 +1,4 @@
+// gulp ajuda na automatização de tarefas que precisam ser feitas dentro o projeto
+
+// gulp e webpack como as duas principais ferramentas de mercado para ajudar nessa manipulacao daos arquivos
+
