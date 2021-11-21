884ae253ffb5494157ddf23b41c02be0aef872f9
LuciLua <luci.lua81@gmail.com>
2021-11-21 12:49:12 -0300

    :star2: [311] - Entendendo Gulp

---
+++ 01/[17] Gulp/311 - entendendoGulp.js
@@ -0,0 +1,26 @@
+ // pc(chrome) ----req---> server
+ // pc(chrome) <---res--- server
+
+// primeira rquisição geralmentepega o html primeiro
+// depois vai pegando os outros... app.js, 
+
+// html tem serie de referencia que vai chamando outros arquivos
+
+
+// se tiver 400 arquivos js, era mts requisiçoes 
+
+/*  GULP:
+
+dev. environment
+[ 300 js , typescript ... ]
+    ||
+    ||
+    ||
+    \/
+prod. environment
+[  10 html, 2 js (typescript => javascript + html)]
+
+automatiza isso com gulp
+
+*/
+
