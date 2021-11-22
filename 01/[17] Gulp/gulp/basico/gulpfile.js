f24bd29cc69044c0195d4579255d6f7f90341a5c
LuciLua <luci.lua81@gmail.com>
2021-11-22 20:29:52 -0300

    :star2: [315] - Fundamentos Gulp 2 | ERRO AO CRIAR PASTA COM GULP

--- 01/[17] Gulp/gulp/basico/gulpfile.js
+++ 01/[17] Gulp/gulp/basico/gulpfile.js
@@ -1,15 +1,45 @@
 const gulp = require('gulp')
+const { src, dest } = require('gulp');
+const uglify = require('gulp-uglify');
 
 // para fazer tasks em serie, uma depois da outra
-// const { series } = require('gulp') 
-const series = gulp.series
+const { series, parallel } = require('gulp') 
+// const series = gulp.series
 
-function copiar(callback){
+const antes1 = cb => {
+    console.log('Tarefa antes 1!')
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa antes 2!\n')
+    return cb()
+}
+
+function copiar(cb){
+    // para selecionar quais arquivos vou selecionar como insumo/entrada para o workflow que sera definido dentrod este arquivo
+    gulp.src(['pastaA/**/*.txt'])
+    // .pipe(uglify())
+    .pipe(dest('pastaB'))
+    // console.log(gulp.src('pastaA/arquivo1.txt'))
     
-    console.log('Tarefa de copiar')
-   
-    return callback()
+    // aplicar trasnformações nos arquivos de entrada definidos... 
+    // .pipe(imagemPelaMetade()) 
+        // .pipe(filtroPretoEmBranco())
+        // .pipe(transformacaoA())
+        // .pipe(transformacaoB())
+        // ...
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa: fim')
+    return cb()
 }
 
 // gulp espera receber  uma tarefa dafault
-module.exports.default = series(copiar)
+module.exports.default = series(
+    parallel(antes1, antes2), // em paralelo
+    copiar,
+    fim
+)
