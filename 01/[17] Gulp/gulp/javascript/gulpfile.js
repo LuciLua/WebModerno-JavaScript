d6949c7722a837c19749e90ebb7c56072fb96601
LuciLua <luci.lua81@gmail.com>
2021-11-23 18:38:47 -0300

    :star2: [317] - Processando JavaScript com gulp

--- 01/[17] Gulp/gulp/javascript/gulpfile.js
+++ 01/[17] Gulp/gulp/javascript/gulpfile.js
@@ -1,11 +1,11 @@
-const { series } = require('gulp')
+const { series, parallel } = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 
-function padrao(cb){
-    gulp.src('src/**/*.js')
+function transformacaoJS(cb){
+    return gulp.src('src/**/*.js')
         .pipe(babel({ 
             comments: false, // para que comentarios nao sejam transferidos para o arquivo final
             presets: ["env"] // env é o preset mais moderno js
@@ -13,12 +13,21 @@ function padrao(cb){
          }))
          .pipe(uglify()) // pega todo o codigo e converte ele para ser minificado
 
+         // para tratar erros
+         .on('error', err => console.log(err))
+
          .pipe(concat('codigo.min.js')) // pega todos os arquivos selecionados que já foram compilados usando babel de uma vesao mais atual para uma versao mais compativel, e concatena 
          // geralmente usa .min quando o codigo é minificado.. parametro: nome do arquivo a ser salvo
 
-         .pipe(gulp.dest('build'))
+         .pipe(gulp.dest('build/js'))
+}
 
+function fim(cb){
+    console.log('Fim!!')
     return cb()
 }
 
-module.exports.default = series(padrao)
+// nem sempre é bom, pois nem sempre executa a que vc precisa primeiro
+// module.exports.default = parallel(transformacaoJS, fim) 
+
+module.exports.default = series(transformacaoJS, fim) 
