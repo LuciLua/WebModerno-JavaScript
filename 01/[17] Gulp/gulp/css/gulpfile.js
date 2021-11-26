4cdafbe821f74e7fd3d30190f1b840054708721b
LuciLua <luci.lua81@gmail.com>
2021-11-26 14:08:43 -0300

    :star2: [322] - Processando SASS com gulp

---
+++ 01/[17] Gulp/gulp/css/gulpfile.js
@@ -0,0 +1,20 @@
+// npm install node-sass gulp-sass --save-dev
+
+const { series } = require('gulp')
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+
+
+function transformacaoCSS() {
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ "uglifyComments": true }))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+exports.default = series(transformacaoCSS)
+
+// so da certo em outro arquivo....
