267703000a7f859426e0bf09261ae3fecdf89185
LuciLua <luci.lua81@gmail.com>
2021-11-27 17:27:51 -0300

    :star2: [326] - SPA Project: Tasks dependencias

--- 01/[17] Gulp/SPA/gulpTasks/deps.js
+++ 01/[17] Gulp/SPA/gulpTasks/deps.js
@@ -1,14 +1,21 @@
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-function depsCSS(cb){
+function depsCSS(){
 
-    return cb()
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+    .pipe(uglifycss({"uglyComments": false}))
+    .pipe(concat('deps.min.css'))
+    .pipe(gulp.dest('build/assets/css'))
+    
 }
 
 
-function depsFONTS(cb){
+function depsFONTS(){
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 
-    return cb()
 }
 
 module.exports = {
