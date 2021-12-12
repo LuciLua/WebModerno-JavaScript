9742dea02282fa201f887744db6d5cd6053e7c53
LuciLua <luci.lua81@gmail.com>
2021-12-12 13:28:04 -0300

    :star2: [339] - Desenvolvimento e Produção

--- 01/[18] Webpack/webpack.config.js
+++ 01/[18] Webpack/webpack.config.js
@@ -4,7 +4,7 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 
@@ -19,15 +19,18 @@ module.exports = {
     },
     optimization: {
         minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true
-            })
+
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({ // funcao construtora
             filename: "estilo.css"
+        }),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
         }),
         new OptimizeCSSAssetsPlugin({})
     ],
