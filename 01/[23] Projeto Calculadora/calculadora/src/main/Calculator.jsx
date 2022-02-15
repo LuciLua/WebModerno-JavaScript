66e1a35412e2c5a9bc41925ef633d4bb16fbb307
LuciLua <luci.lua81@gmail.com>
2022-02-15 15:43:56 -0300

    :star2: [406] Componente Calculator

---
+++ 01/[23] Projeto Calculadora/calculadora/src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import './Calculator.css'
+
+export default class calculator extends Component {
+    render(){
+        return(
+            <div className="calculator">
+                
+            </div>
+        )
+    }
+}
