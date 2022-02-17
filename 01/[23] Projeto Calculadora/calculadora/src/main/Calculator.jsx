e618abc94e0e6625ca184edda9101db0c952f12b
LuciLua <luci.lua81@gmail.com>
2022-02-17 17:15:36 -0300

    :star2: [411] Implementando a Lógica #02

--- 01/[23] Projeto Calculadora/calculadora/src/main/Calculator.jsx
+++ 01/[23] Projeto Calculadora/calculadora/src/main/Calculator.jsx
@@ -29,6 +29,30 @@ export default class calculator extends Component {
 
     setOperation(operation){
         console.log(operation)
+        if (this.state.current  === 0 ){
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === "="
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                // o resultado será o novo indice 0, e o outro numero (indice 1) sera zerado
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e){
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
     
     addDigit(n){
