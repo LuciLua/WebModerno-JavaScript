471f04a7c627dfd3f0f9c418307f1b1c7c3a6bf6
LuciLua <luci.lua81@gmail.com>
2022-02-19 16:53:41 -0300

    :star2: [417] Componente logo

--- 01/[24] Projeto Cadastro de Usuario/crud/frontend/src/components/template/Logo/Logo.jsx
+++ 01/[24] Projeto Cadastro de Usuario/crud/frontend/src/components/template/Logo/Logo.jsx
@@ -1,11 +1,14 @@
 import './Logo.css'
 import React from 'react'
+import logo from '../../../assets/images/logo.png'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
     return(
         <aside className='logo'>
-            Logo
+            <a href="/" className="logo">
+                <img src={logo} alt="logo" />
+            </a>
         </aside>
     )
 }
