a09a9804d7f5ad75ba9b7f861723f7c4b1818dd4
LuciLua <luci.lua81@gmail.com>
2022-02-23 20:02:30 -0300

    :star2: [433] Entendendo o JSX

--- 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/fundamentos/src/pages/index.jsx
+++ 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/fundamentos/src/pages/index.jsx
@@ -1,5 +1,15 @@
 function Home(){
-    return "Home"
+    return(
+        <>
+            <h1>Fundamentos de Next.js e React</h1>
+            <br />
+            <h2>Vamos estudar esse framework!</h2>
+            <br />
+            <hr />
+            <br />
+            <p>Para criar um componente dentro do Next que tenha uma url basta criar arquivo dentro de pages e exportar</p>
+        </>
+    )
 }
 
 export default Home
