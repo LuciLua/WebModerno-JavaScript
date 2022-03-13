0ac2217157454ccfb3000b87c5eaabf624a3f731
LuciLua <luci.lua81@gmail.com>
2022-03-13 20:29:22 -0300

    :star2: [453] Config: NextJS, Tailwind, Firebase (with error: fs)

---
+++ 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/projeto/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // quais as classes que irao utilizar o tailwind
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
