9d8b5bf035b1bb899c4f1ce2c3125f2ed4813cc6
LuciLua <luci.lua81@gmail.com>
2022-03-15 17:36:16 -0300

    :star2: [457] Componente Tabela #2

--- 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/projeto/src/components/Tabela.tsx
+++ 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/projeto/src/components/Tabela.tsx
@@ -10,27 +10,39 @@ export default function Tabela(props) {
     function renderizarCabecalho() {
         return (
             <tr>
-                <th>Codigo</th>
-                <th>Nome</th>
-                <th>Idade</th>
+                <th className={`text-left p-4`}>Codigo</th>
+                <th className={`text-left p-4`}>Nome</th>
+                <th className={`text-left p-4`}>Idade</th>
             </tr>
         )
     }
     function renderizarDados() {
         return props.clientes?.map((cliente, i) => {
-            return(
-                <tr key={cliente.id}>
-                    <td>{cliente.id}</td>
-                    <td>{cliente.nome}</td>
-                    <td>{cliente.idade}</td>
+            return (
+                <tr
+                    key={cliente.id}
+                    className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}
+                >
+                    <td className={`text-left p-4`}>{cliente.id}</td>
+                    <td className={`text-left p-4`}>{cliente.nome}</td>
+                    <td className={`text-left p-4`}>{cliente.idade}</td>
                 </tr>
             )
         })
     }
 
     return (
-        <table>
-            {renderizarCabecalho()}
+        <table className={`
+        w-full
+        rounded-xl
+        overflow-hidden
+        `}>
+            <thead className={`
+            bg-gradient-to-r from-purple-500 to-purple-700
+            text-gray-100
+            `}>
+                {renderizarCabecalho()}
+            </thead>
             {renderizarDados()}
         </table>
     )
