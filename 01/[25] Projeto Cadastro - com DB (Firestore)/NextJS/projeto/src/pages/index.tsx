b87d7fe681757e59a34428a19638c33de0afe7ee
LuciLua <luci.lua81@gmail.com>
2022-03-17 18:54:02 -0300

    :star2: [467] Organizando Código com hooks

--- 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/projeto/src/pages/index.tsx
+++ 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/projeto/src/pages/index.tsx
@@ -2,61 +2,20 @@ import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
-import Cliente from "../core/Cliente";
-
-import { useEffect, useState } from "react";
-import ClienteRepositorio from "../core/ClienteRepositorio";
-import ColecaoCliente from "../backend/db/ColecaoCliente";
+import useClientes from "../hooks/useClientes";
 
 export default function Home() {
 
-  const repo: ClienteRepositorio = new ColecaoCliente()
-
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [clientes, setClientes] = useState<Cliente[]>([])
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-
-
-  // Para alterar estado na inicialização do componente
-  useEffect(obterTodos, [])
-
-
-  function obterTodos() {
-    repo.obterTodos().then(clientes => {
-      //seta os clientes
-      setClientes(clientes)
-      // tabela visivel
-      setVisivel('tabela')
-    })
-  }
-
-  // const clientes = [
-  //   new Cliente('Ana', 32, '1'),
-  //   new Cliente('Bia', 23, '2'),
-  //   new Cliente('Julia', 19, '3'),
-  //   new Cliente('Yasmin', 22, '4')
-  // ]
-
-  function clienteSelecionado(cliente: Cliente) {
-    setCliente(cliente)
-    setVisivel('form')
-  }
-  function clienteExcluido(cliente: Cliente) {
-    console.log("exclui: ", cliente.nome)
-  }
-
-  
-  function novoCliente() {
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
-  
-  async function salvarCliente(cliente: Cliente) {
-    // console.log("salvo:", cliente)
-    await repo.salvar(cliente)
-    obterTodos()
-    // setVisivel('tabela')
-  }
+  const {
+    cliente,
+    clientes,
+    tabelaVisivel,
+    selecionarCliente,
+    excluirCliente,
+    salvarCliente,
+    novoCliente,
+    exibirTabela
+  } = useClientes()
 
   return (
     <div className={`
@@ -66,7 +25,7 @@ export default function Home() {
     items-center
     bg-gradient-to-r from-blue-500 to-purple-500`}>
       <Layout titulo="Cadastro Simples">
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           <>
             <div className="flex justify-end">
               <Botao
@@ -78,15 +37,15 @@ export default function Home() {
             </div>
             <Tabela
               clientes={clientes}
-              clienteSelecionado={clienteSelecionado}
-              clienteExcluido={clienteExcluido}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
             ></Tabela>
           </>
         ) : (
           <Formulario
             cliente={cliente}
             clienteMudou={salvarCliente}
-            cancelado={() => setVisivel('tabela')}
+            cancelado={exibirTabela}
           />
         )}
 
