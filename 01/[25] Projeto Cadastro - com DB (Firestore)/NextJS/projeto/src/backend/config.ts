68c8fe555b70eb78df40b94c411d5428c278624d
LuciLua <luci.lua81@gmail.com>
2022-03-17 18:23:53 -0300

    :star2: [466] Integrando cadastro com Firebase (correcao if)

--- 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/projeto/src/backend/config.ts
+++ 01/[25] Projeto Cadastro - com DB (Firestore)/NextJS/projeto/src/backend/config.ts
@@ -2,20 +2,13 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 // se for zero, ele vai inicializar a aplicacao
-// if (firebase.apps.length) {
-//   firebase.initializeApp({
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   });
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  });
+}
 
-//   //   firebase.firestore()
-// }
-
-firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-});
 
 export default firebase;
