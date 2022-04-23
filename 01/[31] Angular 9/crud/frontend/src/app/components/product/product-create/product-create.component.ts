97402eb82f4a04ef83d08fa6358b5728bd9133c7
LuciLua <luci.lua81@gmail.com>
2022-04-23 18:01:19 -0300

    :star2: [531] Binding

--- 01/[31] Angular 9/crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ 01/[31] Angular 9/crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product-create',
-  templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+  selector: "app-product-create",
+  templateUrl: "./product-create.component.html",
+  styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
+  // vou querer fazer um binding nesse atributo
+  atributoLegal = "valorQualquer";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  fazerAlgo(): void {
+    console.log("Fazendo algo");
   }
-
 }
