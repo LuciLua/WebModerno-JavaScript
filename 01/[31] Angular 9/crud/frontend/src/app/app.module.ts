7bc95cfdfad436be2896aa3e1112264006d6bc93
LuciLua <luci.lua81@gmail.com>
2022-05-06 19:09:59 -0300

    :star2: [544] Componente Atualizar Produto #01

--- 01/[31] Angular 9/crud/frontend/src/app/app.module.ts
+++ 01/[31] Angular 9/crud/frontend/src/app/app.module.ts
@@ -36,7 +36,8 @@ import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
 
 import localePt from '@angular/common/locales/pt';
-import {registerLocaleData} from '@angular/common'
+import {registerLocaleData} from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
 
 registerLocaleData(localePt)
 
@@ -53,6 +54,7 @@ registerLocaleData(localePt)
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
