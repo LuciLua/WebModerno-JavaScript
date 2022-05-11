6a5f20078ae10b558f04fd0b0b5b64f9c24a7b1e
LuciLua <luci.lua81@gmail.com>
2022-05-11 08:58:05 -0300

    :star2: [550] Exibindo Mensagens de Erro

--- 01/[31] Angular 9/crud/frontend/src/app/components/product/product.service.ts
+++ 01/[31] Angular 9/crud/frontend/src/app/components/product/product.service.ts
@@ -32,22 +32,34 @@ export class ProductService {
   }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl);
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   readById(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Product>(url);
+    return this.http.get<Product>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`;
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   delete(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   errorHandler(e: any): Observable<any> {
