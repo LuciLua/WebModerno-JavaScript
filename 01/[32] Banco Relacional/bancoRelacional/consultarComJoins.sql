f76e228bf3a0775eaa639a2866c9869664a8ec23
LuciLua <luci.lua81@gmail.com>
2022-06-02 13:24:48 -0300

    :star2: [574] Explorando os tipos de JOINs

---
+++ 01/[32] Banco Relacional/bancoRelacional/consultarComJoins.sql
@@ -0,0 +1,17 @@
+-- Rodando no workbench
+
+SELECT * FROM prefeitos;
+SELECT * FROM cidades;
+
+SELECT * FROM cidades c INNER JOIN prefeitos p ON c.id = p.cidade_id;
+
+SELECT * FROM cidades c LEFT OUTER JOIN prefeitos p ON c.id = p.cidade_id;
+
+SELECT * FROM cidades c RIGHT JOIN prefeitos p ON c.id = p.cidade_id;
+
+-- SELECT * FROM cidades c FULL JOIN prefeitos p ON c.id = p.cidade_id;
+
+-- Equivalente ao Full join:
+SELECT * FROM cidades c LEFT OUTER JOIN prefeitos p ON c.id = p.cidade_id;
+UNION
+SELECT * FROM cidades c RIGHT JOIN prefeitos p ON c.id = p.cidade_id;
