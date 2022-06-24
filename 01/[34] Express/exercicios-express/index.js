c34d34aa693d02178d41259821ba99e71bc09d40
LuciLua <luci.lua81@gmail.com>
2022-06-24 09:54:08 -0300

    :star2: [601] Configuração e Launcher #02

--- 01/[34] Express/exercicios-express/index.js
+++ 01/[34] Express/exercicios-express/index.js
@@ -6,6 +6,18 @@ const app = express()
 // responsavel para chamar a aplicacao usando node
 // vantagem: monitora os arquivos
 
+// melhor usar o pm2
+// pm2 monit 
+// pm2 status
+// pm2 restart
+// pm2 restart appx-backend
+// pm2 stop 0
+// pm2 kill
+
+// os dois sao launches
+
+// pm2 pra producao, bom pra microserviços tbm
+// nodemon pra desenvolvimento
 
 app.listen(3000, () => {
     console.log("Backend executando!")
