9b1477ae4278f285209a6574d603e9f21931edc8
LuciLua <luci.lua81@gmail.com>
2022-06-25 20:01:56 -0300

    :star2: [602] Tratando a primeira requisição

---
+++ 01/[34] Express/exercicios-express/README.md
@@ -0,0 +1,16 @@
+// nodemon: disparador/launcher
+// responsavel para chamar a aplicacao usando node
+// vantagem: monitora os arquivos
+
+// melhor usar o pm2
+// pm2 monit 
+// pm2 status
+// pm2 restart
+// pm2 restart appx-backend
+// pm2 stop 0
+// pm2 kill
+
+// os dois sao launches
+
+// pm2 pra producao, bom pra microserviços tbm
+// nodemon pra desenvolvimento
--- 01/[34] Express/exercicios-express/index.js
+++ 01/[34] Express/exercicios-express/index.js
@@ -1,23 +1,20 @@
 const express = require('express')
-const nodemon = require('nodemon')
 const app = express()
 
-// nodemon: disparador/launcher
-// responsavel para chamar a aplicacao usando node
-// vantagem: monitora os arquivos
+// so será chamad se a url for /test
+// app.use('/test', (req, res) => {
+//     res.send("I'm fine")
+// })
 
-// melhor usar o pm2
-// pm2 monit 
-// pm2 status
-// pm2 restart
-// pm2 restart appx-backend
-// pm2 stop 0
-// pm2 kill
+// aceita apenas  requisicoes tipo get
+// app.get('/', (req, res) => {
+//     res.send("I'm fine")
+// })
 
-// os dois sao launches
 
-// pm2 pra producao, bom pra microserviços tbm
-// nodemon pra desenvolvimento
+app.use((req, res) => {
+    res.send("I'm fine")
+})
 
 app.listen(3000, () => {
     console.log("Backend executando!")
