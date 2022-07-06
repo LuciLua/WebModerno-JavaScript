b6de54a5cf7ab0c38b6132abd7eb3b69b3ea273b
LuciLua <luci.lua81@gmail.com>
2022-07-06 10:49:55 -0300

    ✅ ok dependencies. app works now

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-final/frontend/src/global.js
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-final/frontend/src/global.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 export const userKey = '__knowledge_user'
 export const baseApiUrl = 'http://localhost:4000'
 
-export function showError(e) {
+export function showError(e) { 
     if(e && e.response && e.response.data) {
         Vue.toasted.global.defaultError({ msg : e.response.data })
     } else if(typeof e === 'string') {
