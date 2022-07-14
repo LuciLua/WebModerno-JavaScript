df0320327e61ad091485401a3aebf3a7af3de87b
LuciLua <luci.lua81@gmail.com>
2022-07-14 08:47:55 -0300

    :star2: [625] API de categoria #01

---
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/api/category.js
@@ -0,0 +1,42 @@
+module.exports = app => {
+    const { existsOrError, notExistsOrError } = app.api.validation
+
+    const save = (req, res) => {
+        const category = { ...req.body }
+        if (req.params.id) category.id = req.params.id
+
+        try {
+            existsOrError(category.name, 'Nome não informado')
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+
+        if (category.id) {
+            app.db('categories')
+                .update(category)
+                .where({ id: category.id })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('categories')
+                .insert(category)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const remove = async (req, res) => {
+        try {
+
+        } catch {
+
+        }
+    }
+
+
+
+}
+
+
+// 4:40
