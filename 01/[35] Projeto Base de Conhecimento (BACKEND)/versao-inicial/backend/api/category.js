525ab2b2d8321953d9760b47e86c9455ba7db223
LuciLua <luci.lua81@gmail.com>
2022-07-15 18:42:42 -0300

    :star2: [625] API de Categoria #01

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/api/category.js
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/api/category.js
@@ -28,15 +28,70 @@ module.exports = app => {
 
     const remove = async (req, res) => {
         try {
+            existsOrError(req.params.id, 'Código da Categoria não foi informado.')
+            const subCategory = await app.db('categories')
+                .where({ parentId: req.params.id })
 
-        } catch {
+            notExistsOrError(subCategory, 'Categoria possui subcategorias.')
 
+            const articles = await app.db('articles')
+                .where({ categoryId: req.params.id })
+            notExistsOrError(articles, 'Categoria possui artigos.')
+
+            const rowsDeleted = await app.db('categories')
+                .where({ id: req.params.id }).del()
+            existsOrError(rowsDeleted, 'Categoria não foi encontrada.')
+
+            res.status(204).send()
+        } catch (msg) {
+            res.status(400).send(msg)
         }
     }
 
 
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter((parent) => parent.id === parentId)
+
+            return parent.length ? parent[0] : null
+        }
+
+        const categoriesWithPath = categories.map((category) => {
+            let path = category.name
+            let parent = getParent(categories, category.parentId)
+
+            while (parent) {
+                path = `${parent.name} > ${path}`
+                parent = getParent(categories, parent.parentId)
+            }
+
+            return { ...category, path }
+
+        })
+
+        categoriesWithPath.sort((a, b) => {
+            if (a.path < b.path) return -1
+            if (a.path > b.path) return 1
+            return 0
+        })
+        return categoriesWithPath
+    }
+
+    const get = (req, res) => {
+        app.db('categories')
+            .then(categories => res.json(withPath(categories)))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getById = (req, res) => {
+        app.db('categories')
+            .where({ id: req.params.id })
+            .first()
+            .then(category => res.json(category))
+            .catch(err => res.status(500).send(err))
+    }
+
+
+    return { save, remove, get, getById }
 
 }
-
-
-// 4:40 
