2d99c8f91b35c195c27ee27c483166d8aae0202a
LuciLua <luci.lua81@gmail.com>
2022-07-17 15:36:48 -0300

    :star2: [627] API de Artigo #01

---
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/api/articles.js
@@ -0,0 +1,103 @@
+const queries = require('./queries')
+
+module.exports = app => {
+    const { existsOrError, notExistsOrError } = app.api.validation
+
+    const save = (req, res) => {
+        const article = { ...req.body }
+        if (req.params.id) article.id = req.params.id
+
+        // validações antes de persistir o artigo
+        try {
+            existsOrError(article.name, 'Nome não informado')
+            existsOrError(article.description, 'Descrição não informada')
+            existsOrError(article.categoryId, 'Categoria não informada')
+            existsOrError(article.userId, 'Autor não informado')
+            existsOrError(article.content, 'Conteúdo não informado')
+        } catch (msg) {
+            // erro do lado do cliente
+            // o front n mandou os atrib necessaios p persistir o artigo
+            res.status(400).send(msg)
+        }
+
+        if (article.id) {
+            app.db('articles')
+                .update(article)
+                .where({ id: article.id })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('articles')
+                .insert(article)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    // diferente de excluir categoria ou usuario
+    const remove = async (req, res) => {
+        try {
+            const rowsDeleted = await app.db('articles')
+                .where({ id: req.params.id }).del()
+
+            // try {
+            existsOrError(rowsDeleted, 'Artigo não foi encontrado.')
+        }
+        catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+        // res.status(204).send()
+        // } catch(msg) {
+        // res.status(500).send(msg)
+        // }
+    }
+
+    const limit = 10 // usado para paginação
+    const get = async (req, res) => {
+        
+        // qual é a pagina | se nao mandou nada: exibe a pagina 1 
+        const page = req.query.page || 1
+
+        // count pra saber quantos registros tem na base de dados
+        // first pega o primeiro registro
+        const result = await app.db('articles').count('id').first()
+        const count = parseInt(result.count)
+
+        app.db('articles')
+            .select('id', 'name', 'description')
+            // offset => deslocamento que vou precisar fazer para trazer os dados
+            .limit(limit).offset(page * limit - limit)
+            .then(articles => res.json({ data: articles, count, limit }))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getById = (req, res) => {
+        app.db('articles')
+            .where({ id: req.params.id })
+            .first()
+            .then(article => {
+                article.content = article.content.toString()
+                return res.json(article)
+            })
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getByCategory = async (req, res) => {
+        const categoryId = req.params.id
+        const page = req.query.page || 1
+        const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
+        const ids = categories.rows.map(c => c.id)
+
+        app.db({ a: 'articles', u: 'users' })
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+            .limit(limit).offset(page * limit - limit)
+            .whereRaw('?? = ??', ['u.id', 'a.userId'])
+            .whereIn('categoryId', ids)
+            .orderBy('a.id', 'desc')
+            .then(articles => res.json(articles))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, remove, get, getById, getByCategory }
+}
