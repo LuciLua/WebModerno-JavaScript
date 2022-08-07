708dc842800de1533f38a5ca3f55866db12c167d
LuciLua <luci.lua81@gmail.com>
2022-08-07 15:24:32 -0300

    :star2: [631] Protegendo a API com Passport

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/config/routes.js
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/config/routes.js
@@ -6,35 +6,43 @@ module.exports = (app) => {
 
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
     app.route('/categories')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.get)
         .post(app.api.category.save)
 
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
 
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.get)
         .post(app.api.article.save)
 
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
         .put(app.api.article.save)
         .delete(app.api.article.remove)
 
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 
     // colocar urls mais especificas mais em cima
