ad2b25a17c201c055537b939cfca19d1b11f0a47
LuciLua <luci.lua81@gmail.com>
2022-08-22 17:30:34 -0300

    :star2: [637] Informações de Conexao no .env

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/config/mongodb.js
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/backend/config/mongodb.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const { stats } = require('../.env')
 // vai implementar conexao com mongo db
 
-mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true })
+mongoose.connect(stats, { useNewUrlParser: true })
     .catch(e => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
