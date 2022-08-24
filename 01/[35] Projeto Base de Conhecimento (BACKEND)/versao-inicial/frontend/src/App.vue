2872212675b88655e96e01b09a361195faffeec9
LuciLua <luci.lua81@gmail.com>
2022-08-24 16:22:01 -0300

    :star2: [639] Estrutura Inicial do Template

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/App.vue
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/App.vue
@@ -1,15 +1,50 @@
 <template>
-	<div id="app">
-		<h1>Versão Inicial</h1>
-	</div>
+  <div id="app">
+    <Header />
+    <Menu />
+    <Content />
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from "@/components/template/Header.vue";
+import Menu from "@/components/template/Menu.vue";
+import Content from "@/components/template/Content.vue";
+import Footer from "@/components/template/Footer.vue";
+
 export default {
-	name: "App",
-}
+  name: "App",
+  components: { Header, Menu, Content, Footer },
+};
 </script>
 
 <style>
+* {
+  font-family: "Lato", sans-serif;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
 
+#app {
+  /* Deixa a renderizacao das fontes mais suave */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  height: 100vh;
+
+  display: grid;
+  /* header 60px, 1fr conteudo da pagina 40px footer */
+  grid-template-rows: 60px 1fr 40px;
+
+  grid-template-columns: 300px 1fr;
+
+  /* primeira linha, segunda linha, terceira linha */
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
 </style>
---
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Cod3r</strong> © 2015-2022</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+.footer {
+  grid-area: footer;
+  background-color: #ddd;
+  color: #333;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding-right: 10px;
+}
+</style>
