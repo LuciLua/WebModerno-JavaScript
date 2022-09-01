519a77cbb407bb348a0e46dd7d31717580a801f4
LuciLua <luci.lua81@gmail.com>
2022-09-01 18:09:33 -0300

    :star2: [645] Componente Home

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/home/Home.vue
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/home/Home.vue
@@ -1,17 +1,59 @@
 <template>
   <div class="home">
     <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de conhecimento" />
+    <div class="stats">
+      <Stat
+        title="Categories"
+        :value="stat.categories"
+        icon="fa fa-folder"
+        color="#d54d50"
+      />
+      <Stat
+        title="Articles"
+        :value="stat.articles"
+        icon="fa fa-file"
+        color="#3bc480cd"
+      />
+      <Stat
+        title="Users"
+        :value="stat.users"
+        icon="fa fa-user"
+        color="#3282d"
+      />
+    </div>
   </div>
 </template>
+  
 
 <script>
 import PageTitle from "../template/PageTitle";
+import Stat from "./Stat.vue";
+import axios from "axios";
+import { baseApiUrl } from "../../global";
 
 export default {
   name: "Home",
-  components: { PageTitle },
+  components: { PageTitle, Stat, axios, baseApiUrl },
+  data: function () {
+    return {
+      stat: {},
+    };
+  },
+  methods: {
+    getStats() {
+      axios.get(`${baseApiUrl}/stats`).then((res) => (this.stat = res.data));
+    },
+  },
+  mounted() {
+    this.getStats();
+  },
 };
 </script> 
-
+  
 <style>
+  .stats{
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 </style>
