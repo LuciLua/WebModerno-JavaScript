38a7697f57b958573159b509f5b7b83d27edf4cc
LuciLua <luci.lua81@gmail.com>
2022-09-02 20:18:12 -0300

    :star2: [647] Cadastro de Usuário #01

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,14 +1,45 @@
 <template>
-    <div class="user-admin">
-      <h1>User Component</h1>
-    </div>
-  </template>
+  <div class="user-admin">
+    <b-table hover striped :items="users" :fields="fields">
+    </b-table>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: "UserAdmin",
-  };
-  </script>
+import { baseApiUrl } from "../../global";
+import axios from "axios";
+
+export default {
+  name: "UserAdmin",
+  data: () => {
+    return {
+      mode: "save",
+      user: {},
+      users: [],
+      // descricao dos fields que serao usados pelo componente b-table
+      fields: [
+      {key: 'id', label: 'Código', sortable: true},
+      {key: 'name', label: 'Nome', sortable: true},
+      {key: 'email', label: 'E-mail', sortable: true},
+      {key: 'admin', label: 'Administrador', sortable: true, formatter: value => value ? 'sim' : 'não'},
+      {key: 'actions', label: 'Ações'},
+    ]
+    };
+  },
+  methods: {
+    loadUsers() {
+      const url = `${baseApiUrl}/users`;
+      axios.get(url).then((res) => {
+        this.users = res.data;
+        // console.log(this.users);
+      });
+    },
+  },
+  mounted() {
+    this.loadUsers();
+  },
+};
+</script>
   
   <style>
-  </style>
+</style>
