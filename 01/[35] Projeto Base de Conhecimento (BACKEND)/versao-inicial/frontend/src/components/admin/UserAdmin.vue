d2177d9429c638421888e91fafadccc6f8d7947b
LuciLua <luci.lua81@gmail.com>
2022-09-04 17:03:14 -0300

    :star2: [650] Cadastro de usuário #02

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -10,6 +10,7 @@
               type="text"
               v-model="user.name"
               required
+              :readonly="mode === 'remove'"
               placeholder="Informe o Nome do Usuário..."
             >
             </b-form-input>
@@ -22,16 +23,22 @@
               type="text"
               v-model="user.email"
               required
+              :readonly="mode === 'remove'"
               placeholder="Informe o E-mail do Usuário..."
             >
             </b-form-input>
           </b-form-group>
         </b-col>
       </b-row>
-      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-2 mb-3">
+      <b-form-checkbox
+        v-show="mode === 'save'"
+        id="user-admin"
+        v-model="user.admin"
+        class="mt-2 mb-3"
+      >
         Administrador?
       </b-form-checkbox>
-      <b-row>
+      <b-row v-show="mode === 'save'">
         <b-col md="6" sm="12">
           <b-form-group label="Senha:" label-for="user-password">
             <b-form-input
@@ -60,21 +67,34 @@
           </b-form-group>
         </b-col>
       </b-row>
-      <b-button
-        variant="primary"
-        v-if="mode == 'save'"
-        @click="save"
-        class="mb-3 mt-3"
-      >
-        Salvar
-      </b-button>
-      <b-button variant="danger" v-if="mode == 'remove'" @click="remove">
-        Excluir
-      </b-button>
-      <b-button @click="reset" class="ml-2"> Cancelar </b-button>
+      <b-row>
+        <b-col xs="12">
+          <b-button
+            variant="primary"
+            v-if="mode == 'save'"
+            @click="save"
+            class="mb-3 mt-3"
+          >
+            Salvar
+          </b-button>
+          <b-button variant="danger" v-if="mode == 'remove'" @click="remove">
+            Excluir
+          </b-button>
+          <b-button @click="reset" class="ml-2"> Cancelar </b-button>
+        </b-col>
+      </b-row>
     </b-form>
     <hr />
-    <b-table hover striped :items="users" :fields="fields"> </b-table>
+    <b-table hover striped :items="users" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
   
@@ -136,10 +156,10 @@ export default {
         })
         .catch(showError);
     },
-    // loadUser(user, mode = "save") {
-    //   this.mode = mode;
-    //   this.user = { ...user };
-    // },
+    loadUser(user, mode = "save") {
+      this.mode = mode;
+      this.user = user;
+    },
   },
   mounted() {
     this.loadUsers();
