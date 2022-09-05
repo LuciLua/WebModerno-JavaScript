56c21ae63af6ec64555258094202cea53abac118
LuciLua <luci.lua81@gmail.com>
2022-09-05 18:30:30 -0300

    :star2: [656] Artigos por categoria #02

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="articles-by-category">
     <PageTitle icon="fa fa-folder-o" :main="category.name" sub="Categoria" />
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        {{ article.name }}
+      </li>
+    </ul>
+    <div class="load-more">
+      <button
+        v-if="loadMore"
+        class="btn btn-lg btn-outline-primary"
+        @click="getArticles">
+        Carregar Mais Artigos
+      </button>
+    </div>
   </div>
 </template>
 
@@ -15,7 +28,7 @@ export default {
   data() {
     return {
       category: {},
-      article: [],
+      articles: [],
       page: 1,
       loadMore: true,
     };
@@ -27,13 +40,34 @@ export default {
         this.category = res.data;
       });
     },
+    getArticles() {
+      const url = `${baseApiUrl}/categories/${this.category.id}/articles?page=${this.page}`;
+      axios(url).then((res) => {
+        this.articles = this.articles.concat(res.data);
+        this.page++;
+
+        if (res.data.length === 0) this.loadMore = false;
+      });
+    },
   },
   mounted() {
     this.category.id = this.$route.params.id;
     this.getCategory();
+    this.getArticles();
   },
 };
 </script>
 
 <style>
+    .articles-by-category ul{
+        list-style-type: none;
+        padding: 0;
+    }
+
+    .articles-by-category .load-more{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 25px;
+    }
 </style>
