3d87fe7dd744fed2e4d32b81fe8d3b153e7e629f
LuciLua <luci.lua81@gmail.com>
2022-09-05 18:20:40 -0300

    :star2: [655] Artigos por categoria #01

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/config/router.js
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/config/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 import Home from '@/components/home/Home'
 import AdminPages from '@/components/admin/AdminPages'
+import ArticlesByCategory from '@/components/article/ArticlesByCategory'
 
 Vue.use(VueRouter)
 
@@ -15,10 +16,16 @@ const routes = [{
     name: 'adminPages',
     path: '/admin',
     component: AdminPages
-}]
+},
+{
+    name: 'ArticlesByCategory',
+    path: '/categories/:id/articles',
+    component: ArticlesByCategory
+},
+]
 
 export default new VueRouter({
     mode: 'history',
     routes: routes
-}) 
+})
 
