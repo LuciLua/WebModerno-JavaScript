fea10ecf81263b25df7bbf81a45adce32f2f3b0b
LuciLua <luci.lua81@gmail.com>
2022-09-06 18:31:50 -0300

    :star2: [659] Menu da Aplicação #01

--- 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/template/Menu.vue
+++ 01/[35] Projeto Base de Conhecimento (BACKEND)/versao-inicial/frontend/src/components/template/Menu.vue
@@ -1,13 +1,51 @@
 <template>
-  <aside class="menu" v-show="isMenuVisible"></aside>
+  <aside class="menu" v-show="isMenuVisible">
+    <div class="menu-filter">
+      <i class="fa fa-search fa-lg"></i>
+      <input
+        type="text"
+        placeholder="Digite para filtrar..."
+        v-model="treeFilter"
+        class="filter-field"
+      />
+    </div>
+    <Tree
+      :data="treeData"
+      :options="treeOptions"
+      :filter="treeFilter"
+      ref="tree"
+    />
+  </aside>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import Tree from "liquor-tree";
+import { baseApiUrl } from "@/global";
+import axios from "axios";
 
 export default {
   name: "Menu",
+  components: { Tree },
   computed: mapState(["isMenuVisible"]),
+  data: function () {
+    return {
+      treeFilter: "",
+      treeData: this.getTreeData(),
+      treeOptions: {
+        propertyNames: { text: "name" },
+        filter: {
+          emptyText: "Categoria não encontrada",
+        },
+      },
+    };
+  },
+  methods: {
+    getTreeData() {
+      const url = `${baseApiUrl}/categories/tree`;
+      return axios.get(url).then((res) => res.data);
+    },
+  },
 };
 </script>
 
@@ -19,5 +57,49 @@ export default {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+}
+
+.menu a,
+.menu a:hover {
+  color: #fff;
+  text-decoration: none;
+}
+
+.menu .tree-node.selected > .tree-content,
+.menu .tree-node > .tree-content:hover {
+  background: rgba(255, 255, 255, 0.2);
+}
+
+.tree-arrow.has-child {
+  filter: brightness(2);
+}
+
+.menu .menu-filter {
+  display: flex;
+  justify-items: center;
+  align-content: center;
+
+  margin: 20px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #AAA;
+}
+.menu .menu-filter i{
+  color: #AAA;
+  /* margin top and right */
+  margin: 10px 10px 0 0; 
+} 
+.menu .menu-filter input {
+  color: #CCC;
+  font-size: 1.3rem;
+  border: 0;
+  width: 100%;
+  outline: none;
+  background: transparent;
+}
+
+.menu .tree-filter-empty{
+  color: #CCC;
+  margin-left: 20px;
+  font-size: 1.2rem;
 }
 </style>
